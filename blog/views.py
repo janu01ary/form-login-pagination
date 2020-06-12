4c13afac33f03f6a086f773791970e7fd6072ca5
aaa <janu01ary@gmail.com>
2020-06-13 03:53:52 +0900

    commit 1

---
+++ blog/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.core.paginator import Paginator
+from .models import Blog
+from .forms import BlogPost
+
+# Create your views here.
+def home(request):  #crud 중 r
+    myBlog = Blog.objects.all()
+    #블로그의 모든 글들을 대상으로
+    blog_list = Blog.objects.all()
+    #블로그 개체 3개를 한 페이지로 자르기
+    paginator = Paginator(blog_list, 3)
+    #request된 페이지가 뭔지를 알아내고 (== request페이지를 변수에 담아)
+    page = request.GET.get('page')
+    #request된 페이지(해당 번호)를 얻어온 뒤 return
+    posts = paginator.get_page(page)
+    return render(request, 'home.html', {'blogs':myBlog, 'posts':posts })
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'detail.html', {'blog':blog_detail})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request): #입력받은 내용을 데이터베이스에 넣어주는 함수
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/' + str(blog.id))
+
+def blogpost(request):  #crud 중 c
+    #1. 입력된 내용을 처리하는 기능 -> POST
+    #2. 빈 페이지를 띄워주는 기능 -> GET
+    if request.method =='POST':
+        form = BlogPost(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)  #아직 저장하지 말고
+            post.pub_date=timezone.now()    #시간 저장하고 나서
+            post.save()     #저장
+            return redirect('home')
+    else:
+        #입력받을 수 있는 form을 띄움
+        form = BlogPost()
+        return render(request,'new.html',{'form':form})
+
+def update(request, pk):
+    #어떤 블로그를 수정할지 해당 블로그 객체 가져오기
+    blog = get_object_or_404(Blog, pk = pk)
+
+    #해당 블로그 객체 pk에 맞는 입력공간 가져오기
+    form = BlogPost(request.POST, instance=blog)
+
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+    return render(request, 'update.html', {'form':form})
+
+def delete(request, pk):
+    myBlog = Blog.objects.get(pk = pk)
+    return render(request, 'sure.html', {'blog':myBlog})
+
+def sure(request, pk):
+    blog = get_object_or_404(Blog, pk = pk)
+    blog.delete()
+    return redirect('home')
---
+++ hw7/urls.py
@@ -0,0 +1,34 @@
+"""hw7 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+import blog.views
+import photo.views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', blog.views.home, name='home'),
+    path('blog/', include('blog.urls')),
+    path('newblog/', blog.views.blogpost, name='newblog'),
+    path('update/<int:pk>', blog.views.update, name='update'),
+    path('delete/<int:pk>', blog.views.delete, name='delete'),
+    path('photo', photo.views.photo, name="photo"),
+    path('accounts/', include('accounts.urls')),
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
